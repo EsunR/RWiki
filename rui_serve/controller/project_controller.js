17f511a21ac175cf388ce14c6e6592e3ffdfba89
EsunR <641411169@qq.com>
2019-10-18 16:22:54 +0800

    调整project数据库

--- rui_serve/controller/project_controller.js
+++ rui_serve/controller/project_controller.js
@@ -1,30 +1,9 @@
 const projectDb = require('../database/project')
-const UserModel = require('../database/user')
+const userDb = require('../database/user')
 
 module.exports = {
   test: async (ctx, next) => {
-    let project = new projectDb.Model({
-      projectInfo: {
-        projectName: "测试项目",
-        creatorUid: "123",
-      }
-    })
-    project.article.push({
-      title: "测试文章",
-      md: "# hello",
-      html: "<h1>hello</h1>"
-    })
-    await project.save().then((result) => {
-      ctx.body = {
-        msg: "ok",
-        data: result
-      }
-    }).catch((err) => {
-      ctx.body = {
-        msg: "新建项目失败",
-        error: err
-      }
-    });
+
   },
   /**
    * 创建一个项目
@@ -32,39 +11,46 @@ module.exports = {
   createProject: async (ctx, next) => {
     const uid = ctx.state.user.uid
     const req = ctx.request.body
-    const userDoc = await UserModel.findById(uid)
+    const userDoc = await userDb.Model.findById(uid)
     let project = new projectDb.Model({
-      projectInfo: {
-        projectName: req.projectName,
-        creator: {
-          uid: userDoc._id,
-          name: userDoc.name
-        },
-        contributors: [],
-        desc: req.desc
-      }
+      // TODO: 添加上传封面
+      projectName: req.projectName,
+      creator: userDoc._id,
+      desc: req.desc
     })
-    await project.save().then((doc) => {
-      ctx.body = {
-        msg: "ok",
-        data: doc
-      }
-    }).catch((err) => {
+    let projectDoc = await project.save().catch((err) => {
       console.log(err);
       ctx.body = {
         msg: "创建项目失败"
       }
     });
+    let projectLinkedDoc = await projectDb.Model
+      .findById(projectDoc._id, { articles: 0 })
+      .populate("creator partners", { name: 1 })
+      .catch(err => {
+        console.log(err);
+        ctx.body = {
+          msg: "读取项目信息失败，手动刷新界面"
+        }
+      })
+    ctx.body = {
+      msg: "ok",
+      data: projectLinkedDoc
+    }
   },
   /**
    * 查找某用户创建的项目
    */
   getProjectListByUid: async (ctx, next) => {
     const uid = ctx.state.user.uid
-    const project = await projectDb.Model.find({ "projectInfo.creator.uid": uid }, "projectInfo")
+    let projectDocs = await projectDb.findCreateProjectByUid(uid)
+      .catch(err => {
+        console.log(err);
+        ctx.body = { msg: "查找项目失败" }
+      })
     ctx.body = {
       msg: "ok",
-      data: project
+      data: projectDocs
     }
   }
 }
